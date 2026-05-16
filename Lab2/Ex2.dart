7c9c727a32ca463bcf62836207abe25beaac27e3
Lunatik137 <nhom096d@gmail.com>
2026-05-16 10:41:24 +0700

    Update add and remove of set and map

--- Lab2/Ex2.dart
+++ Lab2/Ex2.dart
@@ -18,7 +18,7 @@ void main() {
   print("Subtraction: ${a-b}");
 
   // Comparison operators
-  print("a == b: ${a==b}");
+  print("a == b: ${a==b}\n");
 
   /*
   3.	Create a Set (unique values) and a Map (key-value).
@@ -34,22 +34,34 @@ void main() {
     "Cat" : "Meo Meo",
     "Mouse" : "Chit Chit"
   };
-  print("sounds: $sounds");
+  print("sounds: $sounds\n");
 
   /*
   4.	Use indexing, add(), remove(), and map access.
    */
 
   // Indexing
-  print("First number of the list: ${numbers.last}");
+  print("First number of the list: ${numbers.last}\n");
 
   // Add
   numbers.add(23);
   print("List after add: $numbers");
 
+  animals.add("Pig");
+  print("Set after add: $animals");
+
+  sounds["Pig"] = "Oi Oi";
+  print("Map after add: $sounds \n");
+
   // Remove
   numbers.remove(5);
   print("List after remove: $numbers");
+
+  animals.remove("Cat");
+  print("Set after remove: $animals");
+
+  sounds.remove("Dog");
+  print("Map after remove: $sounds \n");
 
   // Map Access
   print("Dog sound: ${sounds["Dog"]}");
