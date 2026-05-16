1abdf59632400685257193e378ddd1406cd90358
Lunatik137 <nhom096d@gmail.com>
2026-05-16 10:30:49 +0700

    Finish Ex2

--- Lab2/Ex2.dart
+++ Lab2/Ex2.dart
@@ -1,7 +1,56 @@
 void main() {
+
+  /*
+  1.	Create a List of integers.
+   */
+
   // List
   List<int> numbers = [1, 5, 3, 4, 5, 6, 6, 7, 3];
 
-  // Arithmetic operators
+  /*
+  2.	Use arithmetic & comparison operators.
+   */
 
+  // Arithmetic operators
+  int a = 13;
+  int b = 5;
+  print("Addition: ${a+b}");
+  print("Subtraction: ${a-b}");
+
+  // Comparison operators
+  print("a == b: ${a==b}");
+
+  /*
+  3.	Create a Set (unique values) and a Map (key-value).
+   */
+
+  // Set
+  Set<String> animals = {"Dog","Cat","Mouse","Cat"};
+  print("animals: $animals");
+
+  // Map
+  Map<String, String> sounds = {
+    "Dog" : "Gau Gau",
+    "Cat" : "Meo Meo",
+    "Mouse" : "Chit Chit"
+  };
+  print("sounds: $sounds");
+
+  /*
+  4.	Use indexing, add(), remove(), and map access.
+   */
+
+  // Indexing
+  print("First number of the list: ${numbers.last}");
+
+  // Add
+  numbers.add(23);
+  print("List after add: $numbers");
+
+  // Remove
+  numbers.remove(5);
+  print("List after remove: $numbers");
+
+  // Map Access
+  print("Dog sound: ${sounds["Dog"]}");
 }
